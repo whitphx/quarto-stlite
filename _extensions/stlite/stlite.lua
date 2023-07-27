840fc7bfcc15007755f7b9a1f58eb91e8c9ab2df
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2023-07-27 16:44:16 +0900

    Create the stlite iframe embedding extension

--- _extensions/stlite/stlite.lua
+++ _extensions/stlite/stlite.lua
@@ -1,6 +1,19 @@
+local stlite_sharing_url = "https://share.stlite.net/"
 
--- Reformat all heading text 
-function Header(el)
-  el.content = pandoc.Emph(el.content)
-  return el
+function CodeBlock(el)
+  if el.attr.classes[1] == "stlite" then
+      encoded_code = pandoc.pipe("python", {"-c", "import sys; import urllib.parse; print(urllib.parse.quote(sys.stdin.read()))"}, el.text)
+
+      src = stlite_sharing_url .. "?embed=true" .. "#code=" .. encoded_code
+
+      quarto.doc.add_html_dependency(
+        {
+          name = "stlite",
+          stylesheets = {"style.css"},
+          scripts = {"iframeResizer.min.js", "stlite.js"},
+        }
+      )
+
+      return pandoc.RawBlock("html", "<iframe src=\"" .. src .. "\" class=\"stlite\"></iframe>")
+  end
 end
