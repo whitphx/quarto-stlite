95f1db8bd2d154d787f7bd6b7fb9d50d54e453af
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2023-07-27 19:17:33 +0900

    Change the target code block language name to \`stlite-python\`

--- _extensions/stlite/stlite.lua
+++ _extensions/stlite/stlite.lua
@@ -1,7 +1,7 @@
 local stlite_sharing_url = "https://share.stlite.net/"
 
 function CodeBlock(el)
-  if string.match(el.attr.classes[1], "python:stlite") then
+  if el.attr and el.attr.classes:find_if(function (c) return string.match(c, "{?stlite%-python}?") end) then
       encoded_code = pandoc.pipe("python", {"-c", "import sys; import urllib.parse; print(urllib.parse.quote(sys.stdin.read()))"}, el.text)
 
       src = stlite_sharing_url .. "?embed=true" .. "#code=" .. encoded_code
